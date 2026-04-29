fb24bd37fb1fd91e60dd5cc8310560d67bb43d66
Farhan <fahanmomin144@proton.me>
2026-04-29 18:29:43 +0530

    Fix missing gestures import, remove unused app.dart, fix test import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:the_easyfit_clinic/main.dart';
+import 'package:easyfit_clinics/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
